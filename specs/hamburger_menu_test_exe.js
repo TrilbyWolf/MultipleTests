4e7fe0618a60f315f21c1da807d938b838bd145c
Braxton Robison <wolfninja1912@gmail.com>
2025-11-10 13:44:23 -0700

    Repo Complete

--- specs/hamburger_menu_test_exe.js
+++ specs/hamburger_menu_test_exe.js
@@ -5,28 +5,28 @@ import MyCartPage from '../pageobjects/mycart.website.js';
 describe('Swag Labs Login Application',()=>{
     it('should login with the correct login info', async()=>{
         await LoginPage.open();
-        await LoginPage.openedValidation();//A
+        await LoginPage.openedValidation();//Postive Test
         await LoginPage.login('standard_user','secret_sauce');
-        await InventoryPage.openedValidation()//A
+        await InventoryPage.openedValidation()//Postive Test
         await InventoryPage.addToCart();
-        await MyCartPage.badgeValidation();//A
+        await MyCartPage.badgeValidation();//Postive Test
         await MyCartPage.clickMyCart();
-        await MyCartPage.openedValidation();//A
+        await MyCartPage.openedValidation();//Postive Test
         await HamburgerMenu.clickHamburgerMenu();
-        await HamburgerMenu.openedValidation();//A
+        await HamburgerMenu.openedValidation();//Postive Test
         await HamburgerMenu.clickResetApp();
-        await MyCartPage.negaBadgeValidation();//A
+        await MyCartPage.negaBadgeValidation();//Postive Test
         await HamburgerMenu.clickAllItems();
-        await InventoryPage.openedValidation();//A
+        await InventoryPage.openedValidation();//Postive Test
         await HamburgerMenu.clickHamburgerMenu();
-        await HamburgerMenu.openedValidation();//A
+        await HamburgerMenu.openedValidation();//Postive Test
         await HamburgerMenu.clickLogout();
-        await LoginPage.openedValidation();//A
+        await LoginPage.openedValidation();//Postive Test
         await LoginPage.login('standard_user','secret_sauce');
-        await InventoryPage.openedValidation();//A
+        await InventoryPage.openedValidation();//Postive Test
         await HamburgerMenu.clickHamburgerMenu();
-        await HamburgerMenu.openedValidation();//A
-        HamburgerMenu.clickAbout();//Runs until page is loaded
-        HamburgerMenu.aboutPageOpenedValidation();
+        await HamburgerMenu.openedValidation();//Postive Test
+        HamburgerMenu.clickAbout();
+        HamburgerMenu.aboutPageOpenedValidation();//Postive Test
     })
 })
--- specs/my_cart_test_exe.js
+++ specs/my_cart_test_exe.js
@@ -4,24 +4,24 @@ import MyCartPage from '../pageobjects/mycart.website.js';
 describe('Swag Labs Login Application',()=>{
     it('should login with the correct login info', async()=>{
         await LoginPage.open();
-        await LoginPage.openedValidation();//A
+        await LoginPage.openedValidation();//Postive Test
         await LoginPage.login('standard_user','secret_sauce');
-        await InventoryPage.openedValidation()//A
+        await InventoryPage.openedValidation()//Postive Test
         await InventoryPage.addToCart();
-        await MyCartPage.badgeValidation();//A
+        await MyCartPage.badgeValidation();//Postive Test
         await MyCartPage.clickMyCart();
-        await MyCartPage.openedValidation();//A
+        await MyCartPage.openedValidation();//Postive Test
         await MyCartPage.clickRemove();
-        await MyCartPage.checkCart();//A
+        await MyCartPage.checkCart();//Postive Test
         await MyCartPage.clickItem();
-        await MyCartPage.inItemValidation();//A
+        await MyCartPage.inItemValidation();//Postive Test
         await MyCartPage.clickMyCart()
-        await MyCartPage.openedValidation();//A
+        await MyCartPage.openedValidation();//Postive Test
         await MyCartPage.clickContinueShopping();
-        await InventoryPage.openedValidation();//A
+        await InventoryPage.openedValidation();//Postive Test
         await MyCartPage.clickMyCart();
-        await MyCartPage.openedValidation();//A
+        await MyCartPage.openedValidation();//Postive Test
         await MyCartPage.clickCheckout();
-        await MyCartPage.checkoutOpenedValidation();//A
+        await MyCartPage.checkoutOpenedValidation();//Postive Test
     })
 })
